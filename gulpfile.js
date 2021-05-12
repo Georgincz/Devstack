7d6dcff2b165a1fc4c6dbd858efd724fb5494ec1
Georgincz <84093565+Georgincz@users.noreply.github.com>
2021-05-12 19:57:02 +0200

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function() {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('minifyCss', function() {
+    return gulp.src('./css/*.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest('./css'));
+});
+
+gulp.task('browserSync', function() {
+    return browserSync.init({server: {baseDir: './'},
+            files: ['*.html','*.css']
+        });
+    }
+);
+
+gulp.task('images', function() {
+    return gulp.src('./images/**/*.+(png|jpg|gif|svg)')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./images'))
+});
+
+gulp.task('uglify', function () {
+    return gulp.src('./js/*.js')
+          .pipe(uglify())
+          .pipe(gulp.dest('./js/min'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.parallel('sass', 'browserSync', 'images', 'uglify'));
---
+++ js/min/script.js
@@ -0,0 +1,1 @@
+document.querySelector(".nadpis").textContent="Nadpis H1 změněný Javascriptem";
